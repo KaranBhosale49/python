c9ab1e716bb78bae02822dd422aa89559a726f42
KaranBhosale49 <92991389+KaranBhosale49@users.noreply.github.com>
2022-06-20 13:26:26 +0530

    20-06-22

---
+++ Practice7.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+myclient = MongoClient("mongodb://localhost:27017/")
+db = myclient["karan"]
+collection = db["mongoPractice"]
+i=1
+for i in range(1,5,1):
+    print("index",i,)
+    id =i
+    name=input("enter name")
+    rollNumber=input("enter rollno")
+    branch=input("enter branch")
+    mylist = { "_id": id, "name": name, "Roll No": rollNumber, "Branch":branch}
+    collection.insert_one(mylist)
+y=collection.find()
+for i in y:
+    print(i)
